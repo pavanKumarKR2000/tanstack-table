715c1c426a0e0345038ae73fd76c961a1eae3957
Pavankumar K R <pavan123456pop@gmail.com>
2023-10-15 23:05:24 +0530

    minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import PeopleDataTable from "./people/data-table";
+import { columns } from "./people/columns";
+import { people } from "@/people";
 
 export default function Home() {
   return (
-    <main>
-      <Button className="text-xl">hello world</Button>
+    <main className="container py-10 mx-auto">
+      <PeopleDataTable columns={columns} data={people} />
     </main>
   );
 }
